96f9979390bb65dddabd26c8152695e9ef4fd77b
vpondtor <virak_pond-tor@brown.edu>
2024-10-22 21:34:34 -0400

    testing commmit

---
+++ Server/cli.c
@@ -0,0 +1,15 @@
+#include <arpa/inet.h>
+#include <inttypes.h>
+#include <netdb.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+// CLI
+// I need an interface for registering commands
+
+int main(void) {
+}
